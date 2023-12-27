2c237fbb8d955f358bf69c2102b7e46c2aaf2495
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-27 12:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+// Function to chop a string into chunks of a specified size
 function stringChop(str, size) {
-  // your code here
+  // Check if the input string is null
+  if (str === null) {
+    return [];
+  }
+
+  // Initialize an empty array to store the chunks
+  const result = [];
+
+  // Loop through the string and create chunks of the specified length
+  for (let i = 0; i < str.length; i += size) {
+    // Use substring to get the chunk of the specified length
+    const chunk = str.substring(i, i + size);
+    result.push(chunk);
+  }
+
+  return result;
 }
 
-// Do not change the code below
+// Prompt the user for input
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
+
+// Display the result using alert
 alert(stringChop(str, size));
